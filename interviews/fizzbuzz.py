5986b82096de77545d2dc5a1df9cd734f696aec5
Sumitra <sumitrac2008@gmail.com>
2021-03-26 10:59:39 -0700

    Frizzbuzz

--- interviews/fizzbuzz.py
+++ interviews/fizzbuzz.py
@@ -3,12 +3,16 @@ def fizzbuzz(number):
     answer = []
 
     for current_number in range(1, number + 1):
-        if current_number % 3 == 0:
+        if current_number % 3 == 0 and current_number % 5 == 0:
+            answer.append("FizzBuzz")
+        elif current_number % 3 == 0:
             answer.append("Fizz")
+        #if current_number % 3 == 0:
+            #answer.append("Fizz")
         elif current_number % 5 == 0:
             answer.append("Buzz")
-        elif current_number % 3 == 0 and current_number % 5 == 0:
-            answer.append("FizzBuzz")
+        #elif current_number % 3 == 0 and current_number % 5 == 0:
+            #answer.append("FizzBuzz")
         else:
             answer.append(f"{current_number}")
     
