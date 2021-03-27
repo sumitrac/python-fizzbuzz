8c150d364a0e2df14b5aa8df3ff41b6419ec5a3d
Sumitra <sumitrac2008@gmail.com>
2021-03-27 11:09:06 -0700

    updated fizzbuzz code

--- interviews/fizzbuzz.py
+++ interviews/fizzbuzz.py
@@ -27,16 +27,3 @@ def fizzbuzz(number):
     #         answer.append(f"{current_number}")
     
     # return answer
-
-
-    for current_number in range(1, number + 1):
-        if current_number % 3 == 0 and current_number % 5 == 0:
-            answer.append("FizzBuzz")
-        elif current_number % 3 == 0:
-            answer.append("Fizz")
-        elif current_number % 5 == 0:
-            answer.append("Buzz")
-        else:
-            answer.append(f"{current_number}")
-    
-    return answer
