076f70940c5cc83b84fe9b2106cdd964d6bdd264
Sumitra <sumitrac2008@gmail.com>
2021-03-27 11:07:54 -0700

    updata fizzbuzz code

--- interviews/fizzbuzz.py
+++ interviews/fizzbuzz.py
@@ -7,12 +7,35 @@ def fizzbuzz(number):
             answer.append("FizzBuzz")
         elif current_number % 3 == 0:
             answer.append("Fizz")
-        #if current_number % 3 == 0:
-            #answer.append("Fizz")
         elif current_number % 5 == 0:
             answer.append("Buzz")
-        #elif current_number % 3 == 0 and current_number % 5 == 0:
-            #answer.append("FizzBuzz")
+        else:
+            answer.append(f"{current_number}")
+    
+    return answer
+
+
+#pracitced using python bebugger
+    # for current_number in range(1, number + 1):
+    #     if current_number % 3 == 0:
+    #         answer.append("Fizz")
+    #     elif current_number % 5 == 0:
+    #         answer.append("Buzz")
+    #     elif current_number % 3 == 0 and current_number % 5 == 0:
+    #         answer.append("FizzBuzz")
+    #     else:
+    #         answer.append(f"{current_number}")
+    
+    # return answer
+
+
+    for current_number in range(1, number + 1):
+        if current_number % 3 == 0 and current_number % 5 == 0:
+            answer.append("FizzBuzz")
+        elif current_number % 3 == 0:
+            answer.append("Fizz")
+        elif current_number % 5 == 0:
+            answer.append("Buzz")
         else:
             answer.append(f"{current_number}")
     
